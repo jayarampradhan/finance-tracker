be7546a230c6ca81e9235e3a0aab066fa1172c15
Jayaram <jayaramimca@gmail.com>
2019-02-03 23:42:35 +0000

    Signup screen mockup
    - Disabling urlrewrie rules, as assumption is web server will do that

--- ui-app/ui-app.gradle.kts
+++ ui-app/ui-app.gradle.kts
@@ -5,9 +5,9 @@ apply {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.tuckey:urlrewritefilter:4.0.3")
+//    implementation("org.tuckey:urlrewritefilter:4.0.3")
 //	compileOnly("javax.servlet:javax.servlet-api:4.0.1")
-	implementation("javax.servlet:javax.servlet-api:4.0.1")
+//	implementation("javax.servlet:javax.servlet-api:4.0.1")
     expectedBy(project(":model"))
 }
 
